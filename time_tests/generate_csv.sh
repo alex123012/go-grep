55829b4ffa4934a7f3960e91bcef8cce5546bcd5
alexey.makhonin <alexey.makhonin@flant.com>
2022-08-21 03:11:37 +0300

    First step to move all tests to makefile

--- time_tests/generate_csv.sh
+++ time_tests/generate_csv.sh
@@ -2,7 +2,19 @@
 set -e
 echo binary,time,mem,disk,files | tee result_table.csv
 rp="/Users/alexmakh"
-paths="$rp/teach $rp/teach/go-grep"
-for path in $paths; do
-    echo $(./test_gnu_exec_from_go.sh $1 $path),$(du -s "$path" | awk '{print $1}'),$(find $path -type f | wc -l | tr -d ' ')
+paths="$rp/teach/go-grep $rp/teach/go-grep/time_tests"
+root_path="$rp/teach/go-grep/time_tests"
+
+binaries="gnu go"
+for binary in $binaries; do
+    make build MOD_PATH="test_${binary}_grep/" binary="grep-${binary}"
 done
+
+for path in $paths; do
+    for binary in $binaries; do
+        echo $(./test_gnu_exec_from_go.sh ${binary} $1 ${path}),$(du -s "$path" | awk '{print $1}'),$(find $path -type f | wc -l | tr -d ' ') | tee -a result_table.csv
+    done
+done
+
+rm_command="rm -f $root_path/grep-go $root_path/grep-gnu $root_path/time-*.txt $root_path/memory-*.txt"
+$rm_command
--- time_tests/test_gnu_exec_from_go.sh
+++ time_tests/test_gnu_exec_from_go.sh
@@ -2,28 +2,15 @@
 
 set -e
 root_path="/Users/alexmakh/teach/go-grep/time_tests"
-rm_command="rm -f $root_path/grep-go $root_path/grep-gnu $root_path/time-*.txt $root_path/memory-*.txt"
-$rm_command
 
-binaries='gnu go'
+binary=$1
 
-for binary in $binaries; do
-    echo "$root_path/test_${binary}_grep/"
-    cd "${root_path}/test_${binary}_grep/"
-    go build -o "${root_path}/grep-${binary}"
-done
 cd "${root_path}"
 for i in {1..20}; do
-    for binary in $binaries; do
-        /usr/bin/time -l bash -c "./grep-${binary} $1 $2 |
-                                  tee -a time-${binary}.txt > /dev/null" 2>&1 |
-                                  awk '/maximum resident set size/{ print $1/1048576 }' |
-                                  tee -a "memory-${binary}.txt" >/dev/null
-    done
+    /usr/bin/time -l bash -c "./grep-${binary} $2 $3 |
+                                tee -a time-${binary}.txt > /dev/null" 2>&1 |
+                                awk '/maximum resident set size/{ print $1/1048576 }' |
+                                tee -a "memory-${binary}.txt" >/dev/null
 done
 
-for binary in $binaries; do
-    echo "${binary},$(awk '{ total += $2; count++ } END { print total/count }' time-${binary}.txt),$(awk '{ total += $1; count++ } END { print total/count }' memory-${binary}.txt)"
-done
-
-# $rm_command
+echo "${binary},$(awk '{ total += $2; count++ } END { print total/count }' time-${binary}.txt),$(awk '{ total += $1; count++ } END { print total/count }' memory-${binary}.txt)"
